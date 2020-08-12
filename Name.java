8c059624c5270377e915daa326e75106c8006c68
Iuliia <iuliia.scheel@awacademyconsultant.de>
2020-08-12 10:33:45 +0200

    hoch zu git

---
+++ Name.java
@@ -0,0 +1,14 @@
+
+public class Name {
+
+	public static void main(String[] args) {
+		
+		String name;
+		byte alter;
+		name = "JULIA";
+		alter = 30;
+		
+		System.out.println("Mein Name ist " + name +" und ich bin " + alter + " Jahre alt" );
+		}
+	}
+
